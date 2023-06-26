47a7d6f1cb3c485860bd80d9f219f6ee6a4c6e3a
mwoolf87 <mwoolfdev@gmail.com>
2023-06-26 17:14:17 -0400

    created static code for components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 
 /* 
 const initialItems = [
@@ -7,8 +7,37 @@ const initialItems = [
 ];
 */
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackagingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
